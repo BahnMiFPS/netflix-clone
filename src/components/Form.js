aff4213702b8f12814fd93f126af07458499395d
BahnMiFPS <87252277+BahnMiFPS@users.noreply.github.com>
2023-03-14 03:03:06 +1100

    remove pass

--- src/components/Form.js
+++ src/components/Form.js
@@ -44,8 +44,8 @@ function Form() {
 	}
 	const formik = useFormik({
 		initialValues: {
-			email: "s@g.c",
-			password: "asdasda",
+			email: "",
+			password: "",
 		},
 		validationSchema: Yup.object({
 			email: Yup.string()
